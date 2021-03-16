b9417d292de8132cb0bb65a376be2ab3a2fd5c7e
tostos <saittosun2@gmail.com>
2021-03-16 21:53:44 +0100

    angular material ilave edildi sort calisiyor

--- src/app/customers/customer-detail/projects/projects.component.ts
+++ src/app/customers/customer-detail/projects/projects.component.ts
@@ -2,28 +2,22 @@ import { LeadFacade } from 'src/app/leads/lead-store/lead.facade';
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatSort, MatSortable } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
-import { Customer } from 'src/app/models/customer';
-import { Observable } from 'rxjs';
-import { map, take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss'],
 })
-export class ProjectsComponent implements OnInit {
-  // customers: Observable<Customer[]> ;
-  customer: Customer;
-  customers: Customer[];
+export class ProjectsComponent implements OnInit, AfterViewInit {
   displayedColumns: string[] = ['projectName', 'status', 'date'];
   // dataSource: MatTableDataSource<Customer>;
   dataSource;
 
   @ViewChild(MatSort) sort: MatSort;
 
-  // ngAfterViewInit() {
-  //   console.log(this.sort);
-  // }
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
 
   // dataSource = new MatTableDataSource(this.customer)
   constructor(private store: LeadFacade) {}
@@ -36,7 +30,6 @@ export class ProjectsComponent implements OnInit {
     this.store.allLeads$.subscribe((results) => {
       if (!results) return;
       this.dataSource = new MatTableDataSource(results);
-      this.dataSource.sort = this.sort;
     });
   }
 }
